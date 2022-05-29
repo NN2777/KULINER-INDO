a1bcb6e020a977b277bff08e6b61b91577374fa7
Abdul_Rohman <rokhman12abdul@gmail.com>
2022-05-30 00:12:40 +0700

    ada ABOUT US

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,30 +1,23 @@
 import React from "react";
-import MultiplePizzas from "../assets/multiplePizzas.jpeg";
+import AboutImg from "../assets/img_about.jpg";
 import "../styles/About.css";
 function About() {
   return (
     <div className="about">
       <div
         className="aboutTop"
-        style={{ backgroundImage: `url(${MultiplePizzas})` }}
+        style={{ backgroundImage: `url(${AboutImg})` }}
       ></div>
       <div className="aboutBottom">
-        <h1> ABOUT US</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
-          officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
-          cumque velit
-        </p>
+        <h1> ABOUT OUR SHOP</h1>
+        <center><p>
+        This shop is a shop that sells various types of Indonesian food 
+        with guaranteed delicious taste and suitable for all Indonesian. 
+        managed by the three of us as founders of the company indo's cafeteria, 
+        consisting of Abdul Rohman, Naufal Nafidiin, and Rima Dhani Aula Ma'rufa. 
+        with determination and consistency we believe 
+        we can provide the best dishes for all of you.
+        </p></center>
       </div>
     </div>
   );
